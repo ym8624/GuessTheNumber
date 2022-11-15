eaca1759a8fec3e805c23de048f16641671f065a
student <student@A26-14>
2022-11-15 10:16:00 +0200

    Add project files.

---
+++ GuessTheNumber/Guess.cs
@@ -0,0 +1,64 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GuessTheNumber
+{
+    public enum Status { Equal, Greater, Less };
+    public class Guess
+    {
+        private int low;
+        private int high;
+        private Random random;
+        private int number;
+        //private int    playerNumber;
+        private int steps;
+
+        public Guess(int low, int high)
+        {
+            random = new Random();
+            this.low = low;
+            this.high = high;
+        }
+
+        public Guess() : this(1, 10) { }
+
+        public int GetSteps()
+        {
+            return steps;
+        }
+        public int GetNumber() => number;
+        public void GuessNumber()
+        {
+            number = random.Next(low, high + 1);
+        }
+
+        public Status PlayerGuess(int playerNumber)
+        {
+            steps++;
+
+            if (number == playerNumber)
+            {
+                return Status.Equal;
+            }
+            else
+            {
+                if (number > playerNumber)
+                {
+                    return Status.Greater;
+                }
+                else
+                {
+                    return Status.Less;
+                }
+            }
+        }
+    }
+}
---
+++ GuessTheNumber/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+
+namespace GuessTheNumber
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private ImageView ivFace;
+        private Button btnTap;
+        private EditText low;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            InitializeViews();
+        }
+
+        private void InitializeViews()
+        {
+            ivFace = FindViewById<ImageView>(Resource.Id.ivFace);
+            ivFace.Click += ivFace_Click;
+            low = FindViewById<EditText>(Resource.Id.low);
+        }
+
+        protected void ivFace_Click(object sender, EventArgs args)
+        {
+            ivFace.SetImageResource(Resource.Drawable.sad);
+        }
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
